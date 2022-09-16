1809cbec750c47665a7d6c824063163f4daee3f2
Niccolo Dejito <ncdejito@gmail.com>
2022-09-16 13:28:59 +0800

    first commit

---
+++ tests.go
@@ -0,0 +1,14 @@
+// Kubernetes
+// If receiver/any node crashes, still received message?
+// If high load, autoscale multiple pods
+
+// Rabbitmq
+// If receiver crashes, message is kept in queue until receiver is back up
+// If queue is full, scales another queue
+// If queue crashes, messages are not lost
+
+// Terraform
+// Is free/how much is this in gcp?
+
+// Redis
+// db crashes, is information saved?
